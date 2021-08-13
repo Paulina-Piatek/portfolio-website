7c1ded2221bc836d5bbcab0d9b3ed98c143ee77f
Paulina Piątek <87935306+Paulina-Piatek@users.noreply.github.com>
2021-08-13 14:21:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -45,8 +45,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("s1").style.top = "0";
+    document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("s1").style.top = "-50px";
+    document.getElementById("navbar").style.top = "-50px";
   }
 }
